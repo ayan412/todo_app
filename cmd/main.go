27ebe790f64458806d58ac63dd0b309fd5fb7b75
ayan412 <mussatayev.ayan@gmail.com>
2022-11-17 08:47:27 +0600

    added handlers.InitRoutes

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"github.com/ayan412/zhashkevych_rest_api/todo-app"
+	"github.com/ayan412/zhashkevych_rest_api/todo-app/pkg/handler"
 	"log"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run(port: "8000"); err != nil {
-		log.Fatalf(format: "error occured while running http server: %s", err.Error())
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
