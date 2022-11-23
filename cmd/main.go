b315ab652a61a839b223e6109385db24084f243f
ayan412 <mussatayev.ayan@gmail.com>
2022-11-23 08:28:51 +0600

    added pkg

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"github.com/ayan412/zhashkevych_rest_api/todo-app"
 	"github.com/ayan412/zhashkevych_rest_api/todo-app/pkg/handler"
+	"github.com/ayan412/zhashkevych_rest_api/todo-app/pkg/repository"
+	"github.com/ayan412/zhashkevych_rest_api/todo-app/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
