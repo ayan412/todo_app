dd57b8c55afcac5e588eb85049b2b57d4c8897cc
ayan412 <mussatayev.ayan@gmail.com>
2022-12-19 22:03:53 +0600

    changed mistypes

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -27,15 +27,15 @@ func (h *Handler) InitRoutes() *gin.Engine {
 		lists := api.Group("/lists")
 		{
 			lists.POST("/", h.createList)
-			lists.GET("/", h.getAllList)
+			lists.GET("/", h.getAllLists)
 			lists.GET("/:id", h.getListById)
 			lists.PUT("/:id", h.updateList)
 			lists.DELETE("/:id", h.deleteList)
 
 			items := lists.Group(":id/items")
 			{
-				items.GET("/", h.createItem)
-				items.POST("/", h.getAllItems)
+				items.POST("/", h.createItem)
+				items.GET("/", h.getAllItems)
 				items.GET("/:item_id", h.getItemById)
 				items.PUT("/:item_id", h.updateItem)
 				items.DELETE("/:item_id", h.deleteItem)
